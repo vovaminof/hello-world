02e6013ed4e68cd1035fd1b1be8ed43734a7c012
Volodymyr Minof <vovaminiof@gmail.com>
2026-02-16 21:36:10 -0500

    fixed path

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("templates/index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
